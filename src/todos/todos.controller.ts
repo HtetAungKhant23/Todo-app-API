afe6dfd4dc1bb50f0c0ccbb793abfe3119e2d714
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-18 14:25:01 +0630

    complete adding api operation

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -4,43 +4,49 @@ import { UserAuthGuard } from "src/auth/auth.guard";
 import { CreateTodoDto } from "./dto/create-todo.dto";
 import { IAuthRequest } from "src/@types/authRequest";
 import { UpdateTodoDto } from "./dto/update-todo.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("todos")
 @ApiTags("Todo")
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
+  @ApiOperation({ summary: "Create Todo" })
   @Post("create")
   @UseGuards(UserAuthGuard)
   async createTodo(@Body() createTodoData: CreateTodoDto, @Request() req: IAuthRequest): Promise<any> {
     return this.todosService.create(createTodoData, req.user.id);
   }
 
+  @ApiOperation({ summary: "Get All Todo" })
   @Get()
   @UseGuards(UserAuthGuard)
   async getAllTodo(@Request() req: IAuthRequest): Promise<any> {
     return this.todosService.findAll(req.user.id);
   }
 
+  @ApiOperation({ summary: "Get Todo by ID" })
   @Get(":id")
   @UseGuards(UserAuthGuard)
   async getTodo(@Param("id") id: string): Promise<any> {
     return this.todosService.findOne(id);
   }
 
+  @ApiOperation({ summary: "Complete Todo" })
   @Post("complete/:id")
   @UseGuards(UserAuthGuard)
   async completeTodo(@Param("id") id: string): Promise<any> {
     return this.todosService.complete(id);
   }
 
+  @ApiOperation({ summary: "Update Todo by ID and it's still uncomplete" })
   @Patch("update/:id")
   @UseGuards(UserAuthGuard)
   async updateTodo(@Param("id") id: string, @Body() updateData: UpdateTodoDto): Promise<any> {
     return this.todosService.update(id, updateData);
   }
 
+  @ApiOperation({ summary: "Delete Todo by ID" })
   @Delete("delete/:id")
   @UseGuards(UserAuthGuard)
   async deleteTodo(@Param("id") id: string): Promise<any> {
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UserAuthGuard } from "src/auth/auth.guard";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @Controller("users")
 @ApiTags("User")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: "Get User List" })
   @Get()
   @UseGuards(UserAuthGuard)
   findAll() {
