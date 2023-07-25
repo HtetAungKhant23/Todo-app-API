15d8465e4a5b1b948438676b87771530d4307a0d
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-25 16:31:25 +0630

    added: adding api property in dto

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateTodoDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
