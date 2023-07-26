6cb05ce701a2d969da204fab3502982d73273410
htetaungkhant <htetaung23.ha@gmail.com>
2023-07-26 16:11:27 +0630

    updated: update cloudinary provider in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from "./auth.service";
 import AuthController from "./auth.controller";
 import { PrismaService } from "src/prisma.service";
 import { JwtService } from "@nestjs/jwt";
+import { CloudinaryProvider } from "src/lib/file.provider";
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, JwtService],
+  providers: [AuthService, PrismaService, JwtService, CloudinaryProvider],
 })
 export class AuthModule {}
